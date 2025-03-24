3d76102290f85031a3bb3be20461ed6be9379d32
johnloser-lwi <john-masterchief@hotmail.com>
2025-03-24 10:59:00 -0700

    finished contact form

---
+++ js/handlemail.js
@@ -0,0 +1,14 @@
+function handleMail() {
+    const clientName = document.getElementById("client-name");
+    const clientEmail = document.getElementById("client-email");
+    const clientMsg = document.getElementById("client-msg");
+
+    const body = `
+    Name: ${clientName.value}
+    Email: ${clientEmail.value}
+
+    ${clientMsg.value}
+    `;
+
+    window.open(`mailto:weiqinyangsound.com?subject=${"Info Requiry"}&body=${body}`);
+}
